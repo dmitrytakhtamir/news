6c368e6b13d814896caa62d9d6c51cc84082b3b5
dmitrytakhtamir <72991969+dmitrytakhtamir@users.noreply.github.com>
2021-04-18 23:59:16 +0300

    Add files via upload

--- guitar_store/urls.py
+++ guitar_store/urls.py
@@ -18,9 +18,16 @@ from django.urls import path
 from content import views
 from django.conf.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('guitars/', include('content.urls', namespace='content')),
 
     path('', views.home, name='home'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
